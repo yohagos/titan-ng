638c80351e9e5bd726c0c8e05975886ef7d87f16
yohagos <yosef.hagos@googlemail.com>
2023-12-24 14:14:04 +0100

    feat: product models and services

--- src/app/core/models/product.model.ts
+++ src/app/core/models/product.model.ts
@@ -6,3 +6,22 @@ export interface ProductFull {
   price: number
   category: CategoryFull
 }
+
+export interface Product {
+  name: string
+  price: number
+  category: CategoryFull
+}
+
+export interface ProductAddRequest {
+  name: string
+  price: number
+  productCategoryId: number
+}
+
+export interface ProductEditRequest {
+  id?: number
+  name?: string
+  price: number
+  category: CategoryFull
+}
--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { JwtService } from './jwt.service';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { ProductAddRequest, ProductEditRequest } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,17 @@ export class ProductService {
 
   loadProducts() {
     return this.http.get('product');
+  }
+
+  addProduct(product: ProductAddRequest) {
+    return this.http.post('product/add', product, {withCredentials: true})
+  }
+
+  editProduct(product: ProductEditRequest) {
+    return this.http.put('product/update', product, {withCredentials: true})
+  }
+
+  deleteProduct(id: number) {
+    return this.http.delete(`product/${id}`)
   }
 }
