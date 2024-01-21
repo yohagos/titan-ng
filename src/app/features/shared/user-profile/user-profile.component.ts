097618969b84f5137573b8fdadc478050ed0b9fe
yohagos <yosef.hagos@googlemail.com>
2024-01-21 18:31:54 +0100

    fix: matCard and Form to display user infos

--- src/app/features/shared/user-profile/user-profile.component.ts
+++ src/app/features/shared/user-profile/user-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { User, UserBasic } from 'src/app/core/models/user.model';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrl: './user-profile.component.scss'
 })
 export class UserProfileComponent {
+  currentUser: User | null
+
+  profileForm: FormGroup
+
+  constructor(
+    private userService: UserService,
+    private formBuilder: FormBuilder,
+  ) {
+    this.currentUser = this.userService.getCurrentUser()
+
+    this.profileForm = this.formBuilder.group({
+      firstname: new FormControl(this.currentUser?.firstname),
+      lastname: new FormControl(this.currentUser?.lastname),
+      email: new FormControl(this.currentUser?.email),
+      role: new FormControl(this.capitalizeFirstLetter(this.currentUser?.role)),
+      pin: new FormControl(this.currentUser?.pin)
+    })
+  }
+
+  capitalizeFirstLetter(value: string | undefined) {
+    if (!value) return value
+    return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase()
+  }
 
 }
