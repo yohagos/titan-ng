5ee64a60fca541b2dfba8c510c2d4c4dc809844c
yohagos <yosef.hagos@googlemail.com>
2024-01-26 17:12:44 +0100

    feat: added utilities

--- src/app/features/shared/services/util.service.ts
+++ src/app/features/shared/services/util.service.ts
@@ -11,4 +11,17 @@ export class UtilService {
     if (!value) return value
     return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase()
   }
+
+  increaseToNextFullNumber(value: number) {
+    if (this.hasDecimalPoints(value)) {
+      return Math.ceil(value);
+    }
+    return value + 1
+  }
+
+  hasDecimalPoints(value: number) {
+    let fractionalPart = value % 1
+    console.log(fractionalPart)
+    return fractionalPart > 0
+  }
 }
