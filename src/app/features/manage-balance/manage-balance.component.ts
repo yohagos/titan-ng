a6d3c12fd016b44011cefb259e36eb83cba3844e
yohagos <yosef.hagos@googlemail.com>
2024-01-29 13:13:15 +0100

    feat: component created

---
+++ src/app/features/manage-balance/manage-balance.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-balance',
+  templateUrl: './manage-balance.component.html',
+  styleUrl: './manage-balance.component.scss'
+})
+export class ManageBalanceComponent {
+
+}
