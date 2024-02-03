ba3cc03a5b0f19940bf854c500d6c4a53f3898c5
yohagos <62908401+yohagos@users.noreply.github.com>
2024-02-03 16:00:10 +0100

    49 product stock (#50)
    * fix: update models
    
    * feat: add productStock service
    
    * feat: update product component
    
    * fix: working on add product dialog
    
    * fix: removed formArray, another issue needs to be implemented first

--- src/app/features/product/add-dialog/add-product-dialog.component.ts
+++ src/app/features/product/add-dialog/add-product-dialog.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { CategoryUnit } from 'src/app/core/models/category.enum';
 import { CategoryFull } from 'src/app/core/models/category.model';
 import { ProductAddRequest } from 'src/app/core/models/product.model';
+import { ProductStock } from 'src/app/core/models/productStock.model';
+import { StorageFull } from 'src/app/core/models/storage.model';
 import { CategoryService } from 'src/app/core/services/category.service';
 import { ProductService } from 'src/app/core/services/product.service';
+import { StorageService } from 'src/app/core/services/storage.service';
 
 @Component({
   selector: 'app-add-dialog',
@@ -13,16 +17,22 @@ import { ProductService } from 'src/app/core/services/product.service';
   styleUrl: './add-product-dialog.component.scss'
 })
 export class AddProductDialogComponent {
+[x: string]: any;
   addProductForm: FormGroup
+  formArray!: FormArray
 
   categories: CategoryFull[] = []
+  inventory: StorageFull[] = []
+
+  categuryUnits = Object.keys(CategoryUnit)
 
   constructor(
     private fb: FormBuilder,
     private categoryService: CategoryService,
     private productService: ProductService,
     public dialogRef: MatDialogRef<AddProductDialogComponent>,
-    public _snackbar: MatSnackBar
+    public _snackbar: MatSnackBar,
+    private storageService: StorageService,
   ) {
     this.addProductForm = this.fb.group({
       name: new FormControl('', Validators.required),
@@ -30,12 +40,31 @@ export class AddProductDialogComponent {
       category: new FormControl()
     })
     this.loadCategoryData()
+    this.loadStorageData()
   }
 
   loadCategoryData() {
     this.categoryService.getCategories().subscribe(
       data => this.categories = data
     )
+  }
+
+  loadStorageData() {
+    this.storageService.getInventory().subscribe(
+      data => this.inventory = data
+    )
+  }
+
+  createRow(): FormGroup {
+    return this.fb.group({
+      measurement: new FormControl(0),
+      unit: new FormControl(CategoryUnit.CL),
+      good: new FormControl(null)
+    })
+  }
+
+  getUnits() {
+    this.categuryUnits.slice(this.categuryUnits.length/2)
   }
 
   addProduct() {
