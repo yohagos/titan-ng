6ef92e53c2334b1c0888d2752e41128d379dcb7a
yohagos <yosef.hagos@googlemail.com>
2024-02-05 13:42:04 +0100

    feat: init edit inventory dialog

---
+++ src/app/features/storage/edit-inventory-dialog/edit-inventory-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-inventory-dialog',
+  templateUrl: './edit-inventory-dialog.component.html',
+  styleUrl: './edit-inventory-dialog.component.scss'
+})
+export class EditInventoryDialogComponent {
+
+}
