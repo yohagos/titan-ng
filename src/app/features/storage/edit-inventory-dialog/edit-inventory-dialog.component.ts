f26e106163898e534c7c578d58ee12eb34f01b23
yohagos <yosef.hagos@googlemail.com>
2024-02-05 14:09:19 +0100

    fix: component for call

--- src/app/features/storage/edit-inventory-dialog/edit-inventory-dialog.component.ts
+++ src/app/features/storage/edit-inventory-dialog/edit-inventory-dialog.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Unit } from 'src/app/core/models/category.enum';
+import { StorageFull } from 'src/app/core/models/storage.model';
+import { StorageService } from 'src/app/core/services/storage.service';
 
 @Component({
   selector: 'app-edit-inventory-dialog',
   templateUrl: './edit-inventory-dialog.component.html',
   styleUrl: './edit-inventory-dialog.component.scss'
 })
-export class EditInventoryDialogComponent {
+export class EditInventoryDialogComponent implements OnInit {
+  editInventoryForm: FormGroup
+
+  units: string[] = []
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private storageService: StorageService,
+    @Inject(MAT_DIALOG_DATA) public data: {inv: StorageFull}
+  ) {
+    this.editInventoryForm = this.formBuilder.group({
+      name: data.inv.name,
+      pricePerBottle: data.inv.pricePerBottle,
+      stockOfBottles: data.inv.stockOfBottles,
+      currentStock: data.inv.currentStock,
+      criticalStockOfBottles: data.inv.criticalStockOfBottles,
+      unit: data.inv.unit,
+      measurement: data.inv.measurement
+    })
+  }
+
+  ngOnInit() {
+    this.units = Object.keys(Unit).filter((item) => {
+      return isNaN(Number(item))
+    })
+  }
 
 }
