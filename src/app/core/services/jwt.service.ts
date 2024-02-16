f95ceb17185edb3b438ec6d77d875154c6f78cd2
yohagos <yosef.hagos@googlemail.com>
2024-02-16 17:37:39 +0100

    update: services

--- src/app/core/services/jwt.service.ts
+++ src/app/core/services/jwt.service.ts
@@ -1,28 +1,45 @@
 import { Injectable } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
+import { environment } from 'src/environments/environment.development';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class JwtService {
+  key = ''
+
+  constructor() {
+    this.key = environment.CRYPT_KEY
+  }
+
+  private encrypt(txt: string) {
+    return CryptoJS.AES.encrypt(txt, this.key).toString()
+  }
+
+  private decrypt(txtToDcrypt: string) {
+    return CryptoJS.AES.decrypt(txtToDcrypt, this.key).toString(CryptoJS.enc.Utf8)
+  }
 
   getToken() {
-    return window.localStorage['jwtToken']
+    return localStorage.getItem('jwtToken')
   }
 
   saveToken(token: string) {
-    window.localStorage['jwtToken'] = token;
+    localStorage.setItem('jwtToken', this.encrypt(token))
   }
 
   getEmail() {
-    return window.localStorage['email']
+    return localStorage.getItem('email')
   }
 
   saveEmail(email: string) {
-    window.localStorage['email'] = email
+    localStorage.setItem('email', email)
   }
 
   destroyToken() {
-    window.localStorage.removeItem('email')
-    window.localStorage.removeItem('jwtToken')
+    localStorage.removeItem('email')
+    localStorage.removeItem('jwtToken')
+    localStorage.removeItem('pin')
   }
 }
