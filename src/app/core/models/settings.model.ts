2b2c571d13d1339acf8fe71a0f599fd7d142ae1a
yohagos <yosef.hagos@googlemail.com>
2024-03-17 17:53:07 +0100

    feat: added settings model

---
+++ src/app/core/models/settings.model.ts
@@ -0,0 +1,16 @@
+export interface Settings {
+  id: number
+
+  companyName: string
+  streetName: string
+  streetNumber: string
+  postalCode: string
+  cityName: string
+
+  customColorTheme: boolean
+  primaryColor: string
+  accentColor: string
+  warnColor: string
+
+  timerLockScreen: number
+}
