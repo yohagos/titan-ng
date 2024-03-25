9b3835e8fa5183d724a5149179173f2d0f480e9b
yohagos <yosef.hagos@googlemail.com>
2024-03-25 14:55:01 +0100

    fix: clean up logs and unused imports

--- src/app/features/table/arrangement/arrangement.component.ts
+++ src/app/features/table/arrangement/arrangement.component.ts
@@ -1,12 +1,9 @@
-import { AfterContentChecked, AfterViewChecked, AfterViewInit, Component, ElementRef, EventEmitter, HostListener, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
-import { CdkDrag, CdkDragDrop, CdkDragEnd, CdkDragEnter, CdkDragMove, CdkDragRelease, CdkDragStart, CdkDropList, CdkDropListGroup, moveItemInArray, transferArrayItem } from "@angular/cdk/drag-drop";
-import { animate, keyframes, style, transition, trigger, state } from "@angular/animations";
+import { AfterViewInit, Component, ElementRef, EventEmitter, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
+import { CdkDragRelease } from "@angular/cdk/drag-drop";
 import { TableService } from 'src/app/core/services/table.service';
-import { TableAdd, TableAddRequest, TableFull } from 'src/app/core/models/table.model';
+import { TableAdd, TableFull } from 'src/app/core/models/table.model';
 import { MatDialog } from '@angular/material/dialog';
 import { AddTableDialogComponent } from './add-table-dialog/add-table-dialog.component';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Observable } from 'rxjs';
 import { SnackbarService } from 'src/app/shared/services/snackbar.service';
 import { EditTableDialogComponent } from './edit-table-dialog/edit-table-dialog.component';
 
@@ -50,11 +47,6 @@ export class ArrangementComponent implements AfterViewInit {
     this.boundaryOffsetTop = this.parentArea.offsetTop
     this.boundaryOffsetHeight = this.parentArea.offsetHeight
     this.boundaryOffsetWidth = this.parentArea.offsetWidth
-
-    /* console.log(this.boundaryOffsetLeft)
-    console.log(this.boundaryOffsetTop)
-    console.log(this.boundaryOffsetHeight)
-    console.log(this.boundaryOffsetWidth) */
   }
 
   loadTables() {
@@ -74,37 +66,16 @@ export class ArrangementComponent implements AfterViewInit {
         const tableNumber = parseInt(item.nativeElement.innerText);
         const table = this.tables.find(t => t.tableNumber === tableNumber);
         if (table) {
-          //const nativeElement = item.nativeElement.getBoundingClientRect();
           const translateX = table.positionX - this.boundaryOffsetLeft
           const translateY = table.positionY - this.boundaryOffsetTop
 
-          if (table) {
-            console.log(table.tableNumber, ' ',table.positionX, ' ', table.positionY)
-          }
-
           this.renderer.setStyle(item.nativeElement, 'left', `${translateX}px`)
           this.renderer.setStyle(item.nativeElement, 'top', `${translateY}px`)
-
-          if (table.tableNumber === 210) {
-            console.log('left: ', item.nativeElement.style['left'])
-            console.log('top: ', item.nativeElement.style['top'])
-          }
         }
       });
     }, 100);
   }
 
-  /* calculateOffsetX(elementRect: DOMRect, parentRect: DOMRect, targetX: number): number {
-    const availableWidth = parentRect.width - elementRect.width;
-    targetX = Math.max(0, Math.min(targetX, availableWidth))
-    return targetX //+ availableWidth / 2
-  }
-
-  calculateOffsetY(elementRect: DOMRect, parentRect: DOMRect, targetY: number): number {
-    const availableHeight = parentRect.height - elementRect.height;
-    targetY = Math.max(0, Math.min(targetY, availableHeight))
-    return targetY //+ availableHeight / 2
-  } */
 
   adjustWidth(num: number) {
     if (num === 2) {
@@ -117,19 +88,11 @@ export class ArrangementComponent implements AfterViewInit {
     this.changesDetected = true
     const mouseEvent: MouseEvent = event.event as MouseEvent
     if (event.source.element) {
-      console.log('Mouseevent: ', mouseEvent.clientX, ' ', mouseEvent.clientY)
       const dom = event.source.element.nativeElement.getBoundingClientRect()
-      /* item.positionX = this.boundaryOffsetLeft + mouseEvent.clientX // - dom.left
-      item.positionY = this.boundaryOffsetTop + mouseEvent.clientY // - dom.top */
-      item.positionX = mouseEvent.clientX // - dom.left
-      item.positionY = mouseEvent.clientY // - dom.top
-
-
-        console.log(item.tableNumber, ' ',item.positionX, ' ', item.positionY)
-
+      item.positionX = mouseEvent.clientX
+      item.positionY = mouseEvent.clientY
     }
     this.tableSet.add(item)
-    console.log(this.tableSet)
   }
 
   addNewTable() {
@@ -138,7 +101,6 @@ export class ArrangementComponent implements AfterViewInit {
     })
     dialogRef.afterClosed().subscribe(
       (data: TableAdd) => {
-
         this.tableService.addTable(data).subscribe({
           next: () => {
             this.tableService.reloadTables()
@@ -167,41 +129,16 @@ export class ArrangementComponent implements AfterViewInit {
 
   updateAllPositions() {
     const tabs = this.tables
-
     for (const table of this.tableSet) {
-      console.log(table)
       const tab = tabs.find(t => t.id === table.id)
       if (tab) {
         tab.positionX = table.positionX
         tab.positionY = table.positionY
-        console.log(tab)
       }
     }
-
     this.tableService.updateTableObservable(tabs)
     setTimeout(() => {
       window.location.reload()
     }, 30)
-
   }
-
-  /* getAllPositions() {
-    const boundary = this.boundary?.nativeElement?.getBoundingClientRect();
-      if (boundary) {
-        this.parentArea = boundary;
-      }
-
-      this.childElements.forEach((item: ElementRef) => {
-        if (item.nativeElement.innerText === '210') {
-          const tableNumber = parseInt(item.nativeElement.innerText);
-          const table = this.tables.find(t => t.tableNumber === tableNumber);
-          if (table) {
-            const nativeElement = item.nativeElement.getBoundingClientRect();
-            const translateX = this.calculateOffsetX(nativeElement, this.parentArea, table.positionX);
-            const translateY = this.calculateOffsetY(nativeElement, this.parentArea, table.positionY);
-            console.log(table.tableNumber, ' ', translateX, ' ', translateY)
-          }
-        }
-      });
-  } */
 }
